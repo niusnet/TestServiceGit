6c543cf3fd7180d52d37240bf8d92f4285b46bb7
Jose Diaz <diaz.jose@c-ven.com>
2023-11-09 23:35:21 -0400

    add

---
+++ CurrentTimeService/Product.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace TestService;
+
+public class Product
+{
+    public Product(int id, string name, int price)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Price = price;
+    }
+
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("price")]
+    public int Price { get; set; }
+}
--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -1,3 +1,5 @@
+using TestService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -11,5 +13,15 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+
+
+var products = new[] { 
+    new Product(1, "Table", 100),
+    new Product(2, "Chair", 200),
+    new Product(3, "Desk", 300),
+    new Product(4, "Computer", 400),
+    new Product(5, "Beer fridge", 500)
+};
+app.MapGet("/products", () => products);
 
 await app.RunAsync();
